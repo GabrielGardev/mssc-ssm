da2c78b53ac9abd41b799c845fdcdbda5663bef0
GabrielGardev <gabriel.gurdev@gmail.com>
2020-09-23 13:56:41 +0300

    adding listener

--- src/main/java/guru/springframework/msscssm/repository/PaymentRepository.java
+++ src/main/java/guru/springframework/msscssm/repository/PaymentRepository.java
@@ -1,7 +1,4 @@
 package guru.springframework.msscssm.repository;
 
-import guru.springframework.msscssm.domain.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-}
+//public interface PaymentRepository extends JpaRepository<Payment, Long> {
+//}
