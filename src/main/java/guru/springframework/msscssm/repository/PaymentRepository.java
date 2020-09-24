5930207c5e650f3c4bcc6576ac1b24da053a2b82
GabrielGardev <gabriel.gurdev@gmail.com>
2020-09-24 15:59:00 +0300

    adding payment service

--- src/main/java/guru/springframework/msscssm/repository/PaymentRepository.java
+++ src/main/java/guru/springframework/msscssm/repository/PaymentRepository.java
@@ -1,4 +1,9 @@
 package guru.springframework.msscssm.repository;
 
-//public interface PaymentRepository extends JpaRepository<Payment, Long> {
-//}
+import guru.springframework.msscssm.domain.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+}
